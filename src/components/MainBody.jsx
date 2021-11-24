4709d05b1fb4164b08f5d21ccf28335b028f69b5
younyikim <rlakuku1221@gmail.com>
2021-11-24 16:41:16 +0900

    :tada: feat : landing page text animation 추가

--- src/components/MainBody.jsx
+++ src/components/MainBody.jsx
@@ -1,23 +1,43 @@
 import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import mainImg from 'assets/main_remove.png';
 
 const MainBody = () => {
   const textRef = useRef();
+  const bgRef = useRef();
+  const textRef2 = useRef();
+  const bgRef2 = useRef();
+  const nameRef = useRef();
 
-  useEffect(() => {});
+  useEffect(() => {
+    let tl = gsap.timeline();
+    tl.to(bgRef.current, 0.3, { scaleX: 1 })
+      .to(bgRef2.current, 0.3, { scaleX: 1 })
+      .to(textRef.current, 0.1, { opacity: 1 }, '-=0.1')
+      .to(textRef2.current, 0.2, { opacity: 1 }, '-=0.1')
+      .to(bgRef.current, 0.2, { scaleX: 0 })
+      .to(bgRef2.current, 0.2, { scaleX: 0 })
+      .to(nameRef.current, 0.4, { opacity: 1 }, '-=0.1');
+  }, []);
 
   return (
     <StyledWrapper>
       <StyledImgContainer>
         <img src={mainImg} alt="" />
       </StyledImgContainer>
-      <StyledDesc ref={textRef}>
-        <span>Front-end</span>
-        <span>Developer</span>
-        <span>Younyi Kim | 김연이</span>
+      <StyledDesc>
+        <div>
+          <span ref={textRef}>Front-end</span>
+          <span ref={bgRef}></span>
+        </div>
+        <div>
+          <span ref={textRef2}>Developer</span>
+          <span ref={bgRef2}></span>
+        </div>
+        <div>
+          <span ref={nameRef}>Younyi Kim | 김연이</span>
+        </div>
       </StyledDesc>
     </StyledWrapper>
   );
@@ -37,6 +57,8 @@ const StyledWrapper = styled.section`
 const StyledDesc = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
   position: relative;
   top: -15rem;
   left: -15rem;
@@ -44,24 +66,67 @@ const StyledDesc = styled.div`
   font-size: 8rem;
   z-index: 10;
   color: white;
+  line-height: 1.205;
 
-  & > span:last-child {
+  & > div:first-child {
+    position: relative;
+
+    & > span:first-child {
+      opacity: 0;
+    }
+
+    & > span:last-child {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: 100;
+      transform-origin: left;
+      transform: scaleX(0);
+      background-color: #f8df8b;
+    }
+  }
+
+  & > div:nth-child(2) {
+    position: relative;
+
+    & > span:first-child {
+      opacity: 0;
+    }
+
+    & > span:last-child {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: 100;
+      transform-origin: left;
+      transform: scaleX(0);
+      background-color: #090910;
+    }
+  }
+  & > div:last-child {
     margin-top: 4rem;
-    font-size: 2.5rem;
-    text-align: right;
+    font-size: 2.8rem;
+
+    & > span {
+      opacity: 0;
+      text-align: right;
+    }
   }
 `;
 
 const StyledImgContainer = styled.div`
   position: relative;
-  width: 570px;
+  width: 50%;
   height: 100%;
-  z-index: 1;
   background-color: #f8df8b;
 
   & > img {
     position: absolute;
-    top: 50%;
+    top: 40%;
     left: -100px;
     width: 130%;
     z-index: 1;

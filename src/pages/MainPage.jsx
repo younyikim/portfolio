3f28f0f7fce298c3e7f7f32e9b397d72ddc4f5c5
younyikim <rlakuku1221@gmail.com>
2021-12-14 10:53:04 +0900

    :tada: feat : 모바일 스크롤링

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -11,6 +11,75 @@ const MainPage = () => {
   const [scrollIndex, setScrollIndex] = useState(1);
 
   useEffect(() => {
+    const touchHandler = (e) => {
+      const { scrollTop } = outerRef.current; // 스크롤 윗쪽 끝부분
+      const pageHeight = window.innerHeight;
+      let end = e.changedTouches[0];
+
+      if (end.screenY - start.screenY > 0) {
+        // 스크롤 올리는 경우
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          // now Page 1
+          outerRef.current.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(1);
+        } else if (scrollTop >= 0 && scrollTop < pageHeight * 2) {
+          // now Page 2
+          outerRef.current.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(1);
+        } else if (scrollTop >= 0 && scrollTop < pageHeight * 3) {
+          // now Page 3
+          outerRef.current.scroll({
+            top: pageHeight,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(2);
+        } else if (scrollTop >= 0 && scrollTop < pageHeight * 4) {
+          // now Page 4
+          outerRef.current.scroll({
+            top: pageHeight * 2,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(3);
+        }
+      } else if (end.screenY - start.screenY < 0) {
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          //now Page 1
+          outerRef.current.scroll({
+            top: pageHeight,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(2);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          //now Page 2
+          outerRef.current.scroll({
+            top: pageHeight * 2,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(3);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 3) {
+          //now Page 3
+          outerRef.current.scroll({
+            top: pageHeight * 3,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setScrollIndex(4);
+        }
+      }
+    };
+
     const wheelHandler = (e) => {
       e.preventDefault();
 
@@ -84,10 +153,21 @@ const MainPage = () => {
     };
     const outerRefCurrent = outerRef.current;
     outerRefCurrent.addEventListener('wheel', wheelHandler);
-    outerRefCurrent.addEventListener('touchmove', wheelHandler);
+
+    // mobile에서의 scroll
+    let start;
+    outerRefCurrent.addEventListener('touchstart', (e) => {
+      start = e.changedTouches[0];
+    });
+    // outerRefCurrent.addEventListener('touchmove', touchHandler);
+    outerRefCurrent.addEventListener('touchend', touchHandler);
     return () => {
       outerRefCurrent.removeEventListener('wheel', wheelHandler);
-      outerRefCurrent.removeEventListener('touchmove', wheelHandler);
+
+      // mobile에서의 scroll event remove
+      outerRefCurrent.removeEventListener('touchstart', touchHandler);
+      outerRefCurrent.removeEventListener('touchmove', touchHandler);
+      outerRefCurrent.removeEventListener('touchend', touchHandler);
     };
   }, []);
 
